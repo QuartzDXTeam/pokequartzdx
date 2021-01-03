29e99d08c4c767dca1a48b3c548d7e87d0b4de95
Mantager <joecann09@gmail.com>
2021-01-03 15:04:54 +0000

    Extend icon palette table

--- src/graphics.c
+++ src/graphics.c
@@ -1542,6 +1542,9 @@ const u16 gMonIconPalettes[][16] =
     INCBIN_U16("graphics/pokemon/icon_palettes/icon_palette_0.gbapal"),
     INCBIN_U16("graphics/pokemon/icon_palettes/icon_palette_1.gbapal"),
     INCBIN_U16("graphics/pokemon/icon_palettes/icon_palette_2.gbapal"),
+    INCBIN_U16("graphics/pokemon/icon_palettes/icon_palette_3.gbapal"),
+    INCBIN_U16("graphics/pokemon/icon_palettes/icon_palette_4.gbapal"),
+    INCBIN_U16("graphics/pokemon/icon_palettes/icon_palette_5.gbapal"),
 };
 
 const u16 gTitleScreenBgPalettes[] = INCBIN_U16("graphics/title_screen/pokemon_logo.gbapal");
