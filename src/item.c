1d3b65f6caaeb2a1d46a5380c27c8d5e52a04faa
Star_Gazr <star.gazaar@gmail.com>
2021-02-02 17:03:20 -0600

    Reusing Repels script

--- src/item.c
+++ src/item.c
@@ -33,6 +33,12 @@ EWRAM_DATA struct BagPocket gBagPockets[POCKETS_COUNT] = {0};
 #include "data/items.h"
 
 // code
+void ItemId_GetHoldEffectParam_Script();
+void ItemId_GetHoldEffectParam_Script()
+{
+    VarSet(VAR_RESULT, ItemId_GetHoldEffectParam(VarGet(VAR_0x8004)));
+}
+
 static u16 GetBagItemQuantity(u16 *quantity)
 {
     return gSaveBlock2Ptr->encryptionKey ^ *quantity;
