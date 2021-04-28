11ae8e90f36bbd40d6d21716dc24d6148af35b21
Star_Gazr <star.gazaar@gmail.com>
2021-04-27 21:09:44 -0500

    shiny rate update

--- include/constants/pokemon.h
+++ include/constants/pokemon.h
@@ -84,7 +84,7 @@
 #define NUM_BATTLE_STATS NUM_STATS + 2 // includes Accuracy and Evasion
 
 // Shiny odds
-#define SHINY_ODDS 8 // Actual probability is SHINY_ODDS/65536
+#define SHINY_ODDS 16 // Actual probability is SHINY_ODDS/65536
 
 // Flags for Get(Box)MonData / Set(Box)MonData
 #define MON_DATA_PERSONALITY        0
